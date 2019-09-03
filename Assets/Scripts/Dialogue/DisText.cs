6c0437a276763e32c4c256841e4511cfdc7784c1
Arami <axg166831@utdallas.edu>
2019-09-03 14:52:39 -0500

    added the text display sequence

---
+++ Assets/Scripts/Dialogue/DisText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisText : MonoBehaviour
+{
+    public TextMesh textComponent;
+    public string text;
+    public float timeLapse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(BuildText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator BuildText()
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            textComponent.text = textComponent.text + text[i];
+            print(text[i]);
+            //Wait a certain amount of time, then continue with the for loop
+            yield return new WaitForSecondsRealtime(timeLapse);
+        }
+    }
+}
