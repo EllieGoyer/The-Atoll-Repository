a61b28166fe6c9966882de5ddc1c526a301e8ce2
JC Steed <djcjdeets@gmail.com>
2019-09-04 20:35:35 -0500

    More footsteps. no audio

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,12 +13,54 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID H_FOOTSTEPS = 3575671711U;
+        static const AkUniqueID JOURNALCLOSE = 3987853926U;
+        static const AkUniqueID JOURNALFLIP = 2690803159U;
+        static const AkUniqueID JOURNALOPEN = 1738215602U;
         static const AkUniqueID LANDSTORM_START = 1930650448U;
         static const AkUniqueID LANDSTORM_STOP = 2869071164U;
+        static const AkUniqueID MENUBACK = 2634859593U;
+        static const AkUniqueID MENUSCROLL = 274288443U;
+        static const AkUniqueID MENUSELECT = 664555880U;
+        static const AkUniqueID PLAYER_FOOTSTEPS = 1730208058U;
         static const AkUniqueID POP_HIT = 3240332696U;
         static const AkUniqueID SEASTORM_START = 875170180U;
         static const AkUniqueID SEASTORM_STOP = 2153236280U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID ROCK = 2144363834U;
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace STATE
+        } // namespace FOOTSTEPS
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID ROCK = 2144363834U;
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
 
     namespace GAME_PARAMETERS
     {
@@ -35,6 +77,11 @@ namespace AK
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID LP_BUS = 1654910302U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
