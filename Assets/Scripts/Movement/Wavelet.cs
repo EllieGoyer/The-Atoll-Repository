19bb33ff24f682bf91ec2013517a63bf1ff43d7b
Walter Han <wappuchino@gmail.com>
2019-09-09 23:53:13 -0500

    A mostly stable ocean sim implementation

---
+++ Assets/Scripts/Movement/Wavelet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Wavelet
+{
+    public Vector2 Wavelength;
+    public Vector2 Velocity;
+    public Vector2 Offset;
+    public float Amplitude;
+    public float Mean;
+}
