5d653482f6e8b1a5cc5509b10d3d8c9391da021a
ORECROS\Drew <orecros@gmail.com>
2019-09-22 17:33:36 -0500

    Added Check and CheckTool components and integrated them with interactables. set up ChoppableTree prefab

---
+++ Assets/Scripts/Inventory/Checks/CheckTool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CheckTool : Check {
+
+    public enum Type { Has, DoesNotHave };
+    public Type toolCheckType;
+    public Tool tool;
+
+    public override bool Perform() {
+        if(toolCheckType == Type.Has) return Inventory.Instance.CheckTool(tool);
+
+        //else if( toolCheckType == Type.DoesNotHave )
+        return !Inventory.Instance.CheckTool(tool);
+    }
+}
