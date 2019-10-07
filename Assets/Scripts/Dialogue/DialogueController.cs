0a28fe859529ed4f1cc12202ec5574230d1acb6f
ORECROS\Drew <orecros@gmail.com>
2019-10-07 17:59:24 -0500

    Added triggers to NPC dialogue trees.

--- Assets/Scripts/Dialogue/DialogueController.cs
+++ Assets/Scripts/Dialogue/DialogueController.cs
@@ -5,24 +5,47 @@ using UnityEngine;
 public class DialogueController : MonoBehaviour
 {
     public Dialogue.DialogueGraph dialogueGraph;
+    bool Active = false;
 
     private void Awake() {
         dialogueGraph.Initialize(this);
     }
 
     private void Update() {
-        if(Input.GetKeyDown(KeyCode.Alpha1)) {
-            dialogueGraph.TryAnswer(0);
+        if(Active) {
+            if (Input.GetKeyDown(KeyCode.Alpha1)) {
+                dialogueGraph.TryAnswer(0);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+                dialogueGraph.TryAnswer(1);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+                dialogueGraph.TryAnswer(2);
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            dialogueGraph.TryAnswer(1);
+        
+    }
+
+    public void Interact() {
+        if(Active) {
+            TryAdvance();
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha3)) {
-            dialogueGraph.TryAnswer(2);
+        else {
+            StartDialogue();
         }
     }
 
     public void TryAdvance() {
         dialogueGraph.TryAdvance();
+    }
+
+    public void StartDialogue() {
+        Active = true;
+        dialogueGraph.Reset();
+    }
+
+    public void EndDialogue() {
+        Debug.Log("The NPC turns their attention away from you");
+        Active = false;
     }
 }
--- Assets/Scripts/Dialogue/Scripts/Editor/DialogueGraphEditor.cs
+++ Assets/Scripts/Dialogue/Scripts/Editor/DialogueGraphEditor.cs
@@ -9,8 +9,19 @@ namespace DialogueEditor {
 	public class DialogueGraphEditor : NodeGraphEditor {
 		
 		public override string GetNodeMenuName(System.Type type) {
-			if (type.Namespace == "Dialogue") return base.GetNodeMenuName(type).Replace("Dialogue/","");
-			else return null;
+            if (type.Namespace == "Dialogue") {
+                string s = base.GetNodeMenuName(type).Replace("Dialogue/", "");
+
+                if(type.IsSubclassOf(typeof(CheckNode))) {
+                    s = "Check/" + s;
+                }
+                else if(type.IsSubclassOf(typeof(TriggerNode))) {
+                    s = "Trigger/" + s;
+                }
+
+                return s;
+            }
+            else return null;
 		}
 	}
 }
