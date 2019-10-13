440f4a9318c8ed8e3be2e3b21fe48a9fc6622a25
ORECROS\Drew <orecros@gmail.com>
2019-10-13 12:32:13 -0500

    added generated sailor & boat clips to all 5 islands

---
+++ Assets/Scripts/Tools/Tools/BoatClipRenderer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatClipRenderer : DrawGizmoSurface {
+    public override Color GetColor() {
+        return Color.green;
+    }
+}
