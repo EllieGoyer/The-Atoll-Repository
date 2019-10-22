da21cd530bd8e67baeedf4c206bf8255fa81f9bf
Drew <orecros@gmail.com>
2019-10-22 14:58:21 -0500

    Added TriggerNotebookEntryNode

---
+++ Assets/Scripts/Dialogue/Scripts/Nodes/TriggerNode/TriggerNotebookEntryNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+namespace Dialogue {
+    public class TriggerModifyToolNode : TriggerNode {
+        public Relationship relationship;
+        public string text;
+        public override void Perform() {
+            Debug.Log("dummy added " + relationship + "notebook text: " + text);
+        }
+    }
+}
