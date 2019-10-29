adc5efab40482d0476861407b8af3e3af4e2edff
Walter Han <wappuchino@gmail.com>
2019-10-28 21:15:22 -0500

    Somewhat broken intersection generator

---
+++ Assets/Scripts/Movement/IntersectionPathCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntersectionPathCollider : PathCollider
+{
+    public float BaseLevel;
+    protected override Vector3[] points
+    {
+        get { return null; }
+    }
+}
