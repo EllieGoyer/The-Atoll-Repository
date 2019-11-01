e81bbf83d2c4f253024e44c5355b131c6bc5953a
Walter Han <wappuchino@gmail.com>
2019-11-01 14:51:29 -0500

    Introducing wind and improved buoyancy

---
+++ Assets/Scripts/Movement/Physics/PlanarWindSurface.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanarWindSurface : WindSurface
+{
+    public Vector2 Size;
+    public Vector3 Center;
+
+    public override float CrossSectionArea(Vector3 windNormal)
+    {
+        Vector3 b = Vector3.ProjectOnPlane(Size.x * transform.right, windNormal);
+        Vector3 h = Vector3.ProjectOnPlane(Size.y * transform.up, windNormal);
+
+        return b.magnitude * h.magnitude;
+    }
+
+    public override float DragCoefficient(Vector3 windNormal)
+    {
+        return Mathf.Lerp(0.005F, 1.28F, CrossSectionArea(windNormal) / (Size.x * Size.y));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawCube(Vector3.zero, new Vector3(Size.x, Size.y, 0.01F));
+    }
+}
---
+++ Assets/Scripts/Movement/Physics/WindSurface.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WindSurface : MonoBehaviour
+{
+    public abstract float CrossSectionArea(Vector3 normal);
+    public abstract float DragCoefficient(Vector3 normal);
+}
