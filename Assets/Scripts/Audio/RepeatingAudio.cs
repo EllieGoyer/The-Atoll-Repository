8f92911df27052e70f4eed853c042a10a5ed0737
Walter Han <wappuchino@gmail.com>
2019-11-04 20:41:31 -0600

    Corrected one shots

--- Assets/Scripts/Audio/RepeatingAudio.cs
+++ Assets/Scripts/Audio/RepeatingAudio.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class RepeatingAudio : AudioInteractable
 {
-    public float Period = 1.0F;
+    public float MinPeriod = 0.5F;
+    public float MaxPeriod = 3.0F;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Play", 0, Period);
+        PlayRepeatingRandom();
+    }
+
+    void PlayRepeatingRandom()
+    {
+        Debug.Log("Play");
+        Play();
+        Invoke("PlayRepeatingRandom", Random.Range(MinPeriod, MaxPeriod));
     }
 }
