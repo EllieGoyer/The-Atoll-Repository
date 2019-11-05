9b3ac097e3e13c0750779135691cadc5cb084ddb
Walter Han <wappuchino@gmail.com>
2019-11-05 16:36:34 -0600

    True beta

---
+++ Assets/TempSwap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempSwap : MonoBehaviour
+{
+    public string First;
+    public string Second;
+    public float Duration = 1;
+
+    public void Swap()
+    {
+        World.CURRENT.ActivePlayer.transform.Find(First).gameObject.SetActive(false);
+        World.CURRENT.ActivePlayer.transform.Find(Second).gameObject.SetActive(true);
+        Invoke("Unswap", Duration);
+    }
+
+    public void Unswap()
+    {
+        World.CURRENT.ActivePlayer.transform.Find(First).gameObject.SetActive(true);
+        World.CURRENT.ActivePlayer.transform.Find(Second).gameObject.SetActive(false);
+    }
+}
